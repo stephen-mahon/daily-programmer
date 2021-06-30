8352af54270bce07f58501e9b5410203f822efb1
Stephen Mahon <stemahon@gmail.com>
2021-06-30 00:18:20 -0500

    init commit fit3

--- 377-Axis-aligned-crate-packing/main.go
+++ 377-Axis-aligned-crate-packing/main.go
@@ -1,7 +1,7 @@
 package main
 
 // To do
-// [ ] fit2
+// [X] fit2
 // [ ] fit3
 // [ ] fitn
 
@@ -15,18 +15,22 @@ type rectangle struct {
 	x, y int
 }
 
+type cuboid struct {
+	x, y, z int
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) == 1 && args[0] == "/help" {
 		//update help with fit2
-		fmt.Println("Given a 2-dimensional rectangular crate of size X by Y, and boxes each of size x by y, returns boxes can fit into a single crate if they have to be placed so that the x-axis of the boxes is aligned with the x-axis of the crate, and the y-axis of the boxes is aligned with the y-axis of the crate")
+		//fmt.Println("Given a 2-dimensional rectangular crate of size X by Y, and boxes each of size x by y, returns boxes can fit into a single crate if they have to be placed so that the x-axis of the boxes is aligned with the x-axis of the crate, and the y-axis of the boxes is aligned with the y-axis of the crate")
 		fmt.Println("Usage: fit1 <X Y x y>")
 		fmt.Println("Example: ./fit1 25 18 6 5")
 	} else {
 		if len(args) != 4 {
 			fmt.Println("You must enter four arguments! Type /help for help.")
 		} else {
-			args := stringToArray(args)
+			args := stringToInt(args)
 			crate, box := crateAndBox(args)
 			fmt.Println("fit1:", fit1(crate, box))
 			fmt.Println("fit2:", fit2(crate, box))
@@ -34,7 +38,7 @@ func main() {
 	}
 }
 
-func stringToArray(args []string) []int {
+func stringToInt(args []string) []int {
 	var argInt []int
 	for _, v := range args {
 		val, err := strconv.Atoi(v)
@@ -44,6 +48,26 @@ func stringToArray(args []string) []int {
 		argInt = append(argInt, val)
 	}
 	return argInt
+}
+
+func maxVal(vals []int) int {
+	max := 0
+	for _, v := range vals {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func rotateBox(box rectangle) rectangle {
+	box.x = box.y
+	box.y = box.x
+	return box
+}
+
+func rotateCuboid(box cuboid, index string) cuboid {
+	return box
 }
 
 func crateAndBox(args []int) (rectangle, rectangle) {
@@ -62,17 +86,5 @@ func fit2(crate, box rectangle) int {
 	var fitArray []int
 	fitArray = append(fitArray, fit1(crate, box))
 	fitArray = append(fitArray, fit1(crate, rotateBox(box)))
-	max := 0
-	for _, v := range fitArray {
-		if v > max {
-			max = v
-		}
-	}
-	return max
-}
-
-func rotateBox(box rectangle) rectangle {
-	box.x = box.y
-	box.y = box.x
-	return box
+	return maxVal(fitArray)
 }

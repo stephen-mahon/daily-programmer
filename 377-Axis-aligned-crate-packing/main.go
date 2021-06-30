763c349884ac710a16fc9bc207e88aa76cb4c009
Stephen Mahon <stemahon@gmail.com>
2021-06-30 00:07:13 -0500

    fit2, added rect struct, added funcs to simplified

--- 377-Axis-aligned-crate-packing/main.go
+++ 377-Axis-aligned-crate-packing/main.go
@@ -11,9 +11,14 @@ import (
 	"strconv"
 )
 
+type rectangle struct {
+	x, y int
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) == 1 && args[0] == "/help" {
+		//update help with fit2
 		fmt.Println("Given a 2-dimensional rectangular crate of size X by Y, and boxes each of size x by y, returns boxes can fit into a single crate if they have to be placed so that the x-axis of the boxes is aligned with the x-axis of the crate, and the y-axis of the boxes is aligned with the y-axis of the crate")
 		fmt.Println("Usage: fit1 <X Y x y>")
 		fmt.Println("Example: ./fit1 25 18 6 5")
@@ -21,36 +26,53 @@ func main() {
 		if len(args) != 4 {
 			fmt.Println("You must enter four arguments! Type /help for help.")
 		} else {
-			fmt.Println(fit1(args))
+			args := stringToArray(args)
+			crate, box := crateAndBox(args)
+			fmt.Println("fit1:", fit1(crate, box))
+			fmt.Println("fit2:", fit2(crate, box))
 		}
 	}
 }
 
-func fit1(args []string) int {
-	var fitArray []int
+func stringToArray(args []string) []int {
+	var argInt []int
 	for _, v := range args {
 		val, err := strconv.Atoi(v)
 		if err != nil {
 			panic(err)
 		}
-		fitArray = append(fitArray, val)
+		argInt = append(argInt, val)
 	}
+	return argInt
+}
 
-	maxX := fitArray[0] / fitArray[2]
-	maxY := fitArray[1] / fitArray[3]
+func crateAndBox(args []int) (rectangle, rectangle) {
+	crate := rectangle{args[0], args[1]}
+	box := rectangle{args[2], args[3]}
+	return crate, box
+}
 
+func fit1(crate, box rectangle) int {
+	maxX := crate.x / box.x
+	maxY := crate.y / box.y
 	return maxX * maxY
 }
 
-func fit2(args []string) int {
+func fit2(crate, box rectangle) int {
 	var fitArray []int
-	// loop to append a slice of fitArray values with the rotated boxes
-	fitArray = append(fitArray, fit1(args))
+	fitArray = append(fitArray, fit1(crate, box))
+	fitArray = append(fitArray, fit1(crate, rotateBox(box)))
 	max := 0
 	for _, v := range fitArray {
 		if v > max {
-			v = max
+			max = v
 		}
 	}
 	return max
+}
+
+func rotateBox(box rectangle) rectangle {
+	box.x = box.y
+	box.y = box.x
+	return box
 }

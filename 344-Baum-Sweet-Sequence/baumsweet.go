8c83c71ecdb873ec98f7adb8930cad7e342ea6f0
Stephen Mahon <stemahon@gmail.com>
2021-07-30 22:04:20 -0500

    complete

---
+++ 344-Baum-Sweet-Sequence/baumsweet.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 1 && args[0] == "/help" {
+		fmt.Println("Baum-Sweet Sequence.")
+	} else {
+		if len(args) != 1 {
+			fmt.Println("You must enter one argument! Type /help for help.")
+		} else {
+			sequence(args[0])
+		}
+	}
+}
+
+func sequence(arg string) {
+	iter, err := strconv.Atoi(string(arg))
+	if err != nil {
+		fmt.Printf("an error occured. expecting an int\nplease try again\n")
+		log.Panic(err)
+	}
+	for i := 0; i <= iter; i++ {
+		val := baumsweet(i)
+		if i != iter {
+			fmt.Printf("%v, ", val)
+		} else {
+			fmt.Printf("%v\n", val)
+		}
+	}
+}
+
+func baumsweet(val int) int {
+	b := fmt.Sprintf("%b", val)
+	var count int
+	var bsArray []int
+	for _, v := range b {
+		if v == '0' {
+			count++
+		} else {
+			bsArray = append(bsArray, count)
+			count = 0
+		}
+
+	}
+	if count != 0 {
+		bsArray = append(bsArray, count)
+	}
+	for _, v := range bsArray {
+		if v%2 != 0 && len(bsArray) > 1 {
+			return 0
+		}
+	}
+	return 1
+}

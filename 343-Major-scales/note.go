2dc181cb8deb611b506f2114ffff33d88fca514c
Stephen Mahon <stemahon@gmail.com>
2021-08-19 22:30:54 -0500

    fixed edge cases

--- 343-Major-scales/note.go
+++ 343-Major-scales/note.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,7 +13,7 @@ func main() {
 		fmt.Println("Major scales.")
 		fmt.Println("This programs takes the name of a major scale and the solfège name of a note, and returns the corresponding note in that scale.")
 		fmt.Println("The movable do solfège system are referred to by the names Do, Re, Mi, Fa, So, La, and Ti, respectively.")
-		fmt.Println("The 12 notes in the chromatic are: C  C#  D  D#  E  F  F#  G  G#  A  A#  B")
+		fmt.Println("The 12 notes in the chromatic scale are: C  C#  D  D#  E  F  F#  G  G#  A  A#  B")
 		fmt.Println("Usage: ./note <string1, string2>")
 		fmt.Println("Example: ./note D Mi")
 		fmt.Println("Output: F#")
@@ -23,30 +24,33 @@ func main() {
 			fmt.Println(note(args))
 		}
 	}
-
 }
 
 func reorder(note string, notes []string) []string {
+
 	for i := range notes {
-		if note == notes[i] {
+		if notes[i] == note {
 			return append(notes[i:], notes[:i]...)
 		}
 	}
+
+	fmt.Printf("error! you must use the chromatic scale you entered %q. see /help for more info.\n", note)
+	log.Fatal()
 	return notes
 }
 
 func note(args []string) string {
-	note := args[0]
-	solfage := args[1]
+	note := strings.ToUpper(args[0])
+	solfage := strings.ToLower(args[1])
 
 	scale := map[string]int{
-		"Do": 0,
-		"Re": 2,
-		"Mi": 4,
-		"Fa": 5,
-		"So": 7,
-		"La": 9,
-		"Ti": 11,
+		"do": 0,
+		"re": 2,
+		"mi": 4,
+		"fa": 5,
+		"so": 7,
+		"la": 9,
+		"ti": 11,
 	}
 
 	notes := []string{
@@ -56,9 +60,9 @@ func note(args []string) string {
 	notes = reorder(note, notes)
 	v, ok := scale[solfage]
 	if ok != true {
-		fmt.Printf("error! you must use the the movable do solfège system. you entered %q. see /help for more info.\n", solfage)
+		fmt.Printf("error! you must use the movable do solfège system. you entered %q. see /help for more info.\n", solfage)
 		log.Fatal()
 	}
-	return notes[v]
 
+	return notes[v]
 }

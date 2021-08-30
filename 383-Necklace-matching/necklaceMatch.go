fbb9b40a1f6828ec61dc2e5878c0b2e8d71f8b0d
Stephen Mahon <stemahon@gmail.com>
2021-08-29 20:54:18 -0500

    bonus 2 completed

--- 383-Necklace-matching/necklaceMatch.go
+++ 383-Necklace-matching/necklaceMatch.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -11,18 +14,21 @@ func main() {
 		fmt.Println("Same Necklace.")
 		fmt.Println("Example: ./sameNecklace nicole icolen")
 		fmt.Println("Output: true")
+	} else if args[0] == "Bonus" {
+		getLinktoFile("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt", "enable1.txt")
+		bonus()
 	} else {
 		if len(args) != 2 {
 			fmt.Println("You must enter two arguments! Type -help for help.")
 		} else {
-			fmt.Println(sameNecklace(args))
+			word := args[0]
+			check := args[1]
+			fmt.Println(sameNecklace(word, check))
 		}
 	}
 }
 
-func sameNecklace(args []string) bool {
-	word := args[0]
-	check := args[1]
+func sameNecklace(word, check string) bool {
 	for i := range word {
 		if check == word[i:]+word[:i] {
 			return true
@@ -43,4 +49,58 @@ func repeats(word string) int {
 		}
 	}
 	return num
+}
+
+func getLinktoFile(webpage, filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	page, err := http.Get(webpage)
+	if err != nil {
+		panic(err)
+	}
+	content, err := ioutil.ReadAll(page.Body)
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString(string(content))
+}
+
+// thanks to user/skeeto
+func bonus() {
+	file, err := os.Open("enable1.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	fmt.Printf("\nNecklace Match\n** Bonus Challenge **\n")
+
+	list := make(map[string][]string)
+	for scanner.Scan() {
+		word := scanner.Text()
+		ordw := canonicalize(word)
+		list[ordw] = append(list[ordw], word)
+	}
+
+	for _, words := range list {
+		if len(words) == 4 {
+			fmt.Printf("%v\n", words)
+			break
+		}
+	}
+}
+
+func canonicalize(s string) string {
+	c := s + s[:len(s)-1]
+	best := s
+	for i := 1; i < len(s); i++ {
+		if c[i:i+len(s)] < best {
+			best = c[i : i+len(s)]
+		}
+	}
+	return best
 }

b36858257f17afa3ad83f3463011ee962b28e207
Stephen Mahon <stemahon@gmail.com>
2021-08-30 20:03:26 -0500

    challenge complete

--- 341-Repeating-Numbers/repeatingNumbers.go
+++ 341-Repeating-Numbers/repeatingNumbers.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+// To do - error check on number fails when input is greater than int
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Repeating numbers")
+	args := os.Args[1:]
+	if len(args) == 1 && args[0] == "-help" {
+		fmt.Println("Repeating numbers.")
+	} else {
+		if len(args) != 1 {
+			fmt.Println("You must enter one arguments! Type -help for help.")
+		} else {
+			repeatingNumbers(args[0])
+		}
+	}
+}
+
+func repeatingNumbers(arg string) {
+	_, err := strconv.Atoi(arg)
+	if err != nil {
+		fmt.Printf("error, expecting a number but recieved %q\n", arg)
+		log.Fatal()
+	}
+
+	m := make(map[string]int)
+
+	for i := 0; i < len(arg)-1; i++ {
+		for j := 0; j < len(arg)-(i+1); j++ {
+			m[arg[i:len(arg)-j]]++
+		}
+	}
+
+	for index, val := range m {
+		if val == 1 {
+			delete(m, index)
+		}
+	}
+	if len(m) == 0 {
+		fmt.Println(0)
+	} else {
+		for i, v := range m {
+			fmt.Printf("%v:%v ", i, v)
+		}
+		fmt.Println()
+	}
+
 }

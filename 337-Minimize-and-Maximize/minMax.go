3d12626396a68a64c49c81facb543ff52c559f58
Stephen Mahon <stemahon@gmail.com>
2021-09-09 22:49:45 -0500

    challenge 1 solved

--- 337-Minimize-and-Maximize/minMax.go
+++ 337-Minimize-and-Maximize/minMax.go
@@ -6,13 +6,13 @@ import (
 )
 
 var message = "Minimize and Maximize"
-var pi = 3.14
-var length = 100.0
 var gr = (math.Sqrt(5) + 1) / 2
-var tol = 1e-5
+var tol = 1e-1
 
 func main() {
-	fmt.Println(message)
+	fmt.Printf("%v\n\n", message)
+	fmt.Printf("Challenge 1\nA piece of wire 100 cm in length is bent into the shape of a sector of a circle. Find the angle of the sector that maximizes the area A of the sector.\n")
+	fmt.Printf("%.1f\n", gss(0, 180, tol))
 }
 
 // Golden-section search
@@ -25,7 +25,7 @@ func gss(a, b, tol float64) float64 {
 	d := a + (b-a)/gr
 
 	for math.Abs(b-a) > tol {
-		if f(c) < f(d) {
+		if f(c) > f(d) {
 			b = d
 		} else {
 			a = c
@@ -36,6 +36,19 @@ func gss(a, b, tol float64) float64 {
 	return (b + a) / 2
 }
 
-func f(val float64) float64 {
-	return -1
+/*
+	Perimeter of Sector = 2r + 2 pi r (theta/360) = 2r(1 + pi(theta/360))
+	Perimeter = 100 cm
+	->	2r (1 + pi (theta/360)) = 100
+		r = 100 / 2(1 + pi (theta/360))
+
+	Area = (theta/360) pi r^2
+	In terms of only theta
+	Area = (theta/360) pi (100 / 2(1 + pi (theta/360)))^2
+	Area = (theta/360) pi 10000 / 4(1 + 2 pi (theta/360) + (theta/360)^2)
+*/
+func f(theta float64) float64 {
+
+	return ((theta / 360) * math.Pi * 10000.0) / (4.0 * (1 + 2.0*math.Pi*(theta/360) + math.Pi*(theta/360)*math.Pi*(theta/360)))
+
 }

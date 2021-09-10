09737ee2d81e9d59a0b96f2a0ccb510fb4b5fe0c
Stephen Mahon <stemahon@gmail.com>
2021-09-09 21:34:25 -0500

    defined Golden-section search. Need function

--- 337-Minimize-and-Maximize/minMax.go
+++ 337-Minimize-and-Maximize/minMax.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 var message = "Minimize and Maximize"
+var pi = 3.14
+var length = 100.0
+var gr = (math.Sqrt(5) + 1) / 2
+var tol = 1e-5
 
 func main() {
 	fmt.Println(message)
+}
+
+// Golden-section search
+/*
+	to find the minimum of f on [a, b]
+	f: a strictly unimodal function on [a,b]
+*/
+func gss(a, b, tol float64) float64 {
+	c := b - (b-a)/gr
+	d := a + (b-a)/gr
+
+	for math.Abs(b-a) > tol {
+		if f(c) < f(d) {
+			b = d
+		} else {
+			a = c
+		}
+		c = b - (b-a)/gr
+		d = a + (b-a)/gr
+	}
+	return (b + a) / 2
+}
+
+func f(val float64) float64 {
+	return -1
 }

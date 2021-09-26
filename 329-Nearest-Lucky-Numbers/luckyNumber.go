aba26b1d174a28bb92afa7fd410000933ad2b5f5
Stephen Mahon <stemahon@gmail.com>
2021-09-25 20:34:04 -0500

    some progress but not working

--- 329-Nearest-Lucky-Numbers/luckyNumber.go
+++ 329-Nearest-Lucky-Numbers/luckyNumber.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 var title = "Lucky numbers"
@@ -16,14 +18,42 @@ func main() {
 	} else if len(args) == 1 && args[0] == "-help" {
 		fmt.Println(title)
 		fmt.Printf(help)
-	} else if len(args) > 1 {
-		luckyNums(args)
-	} else {
+	} else if len(args) != 1 {
 		fmt.Println(err)
+	} else {
+		vals := numSet(args)
+		luckyNums(vals)
 	}
 }
 
-func luckyNums(args []string) {
-	fmt.Println(args)
+func numSet(args []string) []int {
+	limit, err := strconv.Atoi(args[0])
+	var setOfNum []int
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 1; i <= limit; i++ {
+		setOfNum = append(setOfNum, i)
 
+	}
+	return setOfNum
+}
+
+func luckyNums(set []int) []int {
+
+	fmt.Println(set, set[1])
+
+	if set[1] > len(set) {
+		return set
+	}
+
+	var vals []int
+	for i, v := range set {
+		if i%(set[1]) != 0 {
+			vals = append(vals, v)
+		}
+
+	}
+
+	return luckyNums(vals)
 }

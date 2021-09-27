a69866ab950c8fc4b4ab55b6bbbba6042d101026
Stephen Mahon <stemahon@gmail.com>
2021-09-26 21:13:39 -0500

    working algorithm but need nextLuckyNum func

--- 329-Nearest-Lucky-Numbers/luckyNumber.go
+++ 329-Nearest-Lucky-Numbers/luckyNumber.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 var title = "Lucky numbers"
 var err = "See readme"
 var help = ""
+var count int
 
 func main() {
 	args := os.Args[1:]
@@ -21,39 +23,66 @@ func main() {
 	} else if len(args) != 1 {
 		fmt.Println(err)
 	} else {
-		vals := numSet(args)
-		luckyNums(vals)
-	}
-}
-
-func numSet(args []string) []int {
-	limit, err := strconv.Atoi(args[0])
-	var setOfNum []int
-	if err != nil {
-		log.Fatal(err)
-	}
-	for i := 1; i <= limit; i++ {
-		setOfNum = append(setOfNum, i)
-
-	}
-	return setOfNum
-}
-
-func luckyNums(set []int) []int {
-
-	fmt.Println(set, set[1])
-
-	if set[1] > len(set) {
-		return set
-	}
-
-	var vals []int
-	for i, v := range set {
-		if i%(set[1]) != 0 {
-			vals = append(vals, v)
+		val, err := input(args[0])
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Println(isLucky(val))
+	}
+}
+
+func input(arg string) (int, error) {
+	lim, err := strconv.Atoi(arg)
+	if err != nil {
+		err = errors.New("expecting an number but recieved: \"" + arg + "\"")
+	}
+	return lim, err
+}
+
+func genArray(val int) []int {
+	var vals []int
+	for i := 1; i <= val; i++ {
+		vals = append(vals, i)
+	}
+
+	return vals
+}
+
+func luckyNums(s []int, count int) ([]int, int) {
+	if s[count] >= len(s) {
+		return s, count
+	}
+	var vals []int
+	if count == 0 {
+		for i := 0; i < len(s); i += 2 {
+			vals = append(vals, s[i])
+		}
+	} else {
+		for j := 1; j < len(s)+1; j++ {
+			i := j - 1
+			if j%s[count] != 0 {
+				vals = append(vals, s[i])
+			}
+		}
+	}
+
+	count++
+	return luckyNums(vals, count)
+}
+
+func isLucky(val int) string {
+	vals := genArray(val)
+	count := 0
+	vals, _ = luckyNums(vals, count)
+	luckyNum := vals[len(vals)-1]
+
+	if val == luckyNum {
+		return fmt.Sprintf("%v is a lucky number", val)
+
+	} else {
+		var nextLuckNum int
+		return fmt.Sprintf("%v < %v < %v", luckyNum, val, nextLuckNum)
 
 	}
 
-	return luckyNums(vals)
 }

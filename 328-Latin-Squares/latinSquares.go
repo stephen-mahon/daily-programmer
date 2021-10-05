add1cd58486c8ac65e63ad3bbc61fa00a6737324
Stephen Mahon <stemahon@gmail.com>
2021-10-04 23:56:23 -0500

    2nd test fail.

--- 328-Latin-Squares/latinSquares.go
+++ 328-Latin-Squares/latinSquares.go
@@ -3,8 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
 	"strings"
 )
+
+/*
+	**bug 1**
+	second test fail
+		> -length=2 -array="1 3 3 4"
+		> [1 3]
+		> [3 4]
+		>
+		> true
+
+	I suspect its due to the rows and cols being identical. The test must be for a list of number in order. Or check non matching rows and columns for instance R0 && C1 == 0.
+*/
 
 var title = "Latin squares"
 
@@ -13,23 +26,30 @@ type grid struct {
 	y int
 }
 
+type sortRunes []rune
+
 func main() {
 	fmt.Println(title)
 	length := flag.Int("length", 5, "the length, n, of the array.")
 	array := flag.String("array", "1 2 3 4 5 5 1 2 3 4 4 5 1 2 3 3 4 5 1 2 2 3 4 5 1", "the array matrix in the form n x n where n is the length of the array.")
 	flag.Parse()
 
-	square := arrayPrint(length, array)
-	for _, v := range square {
-		fmt.Println(v)
-	}
+	square := parseArray(length, array)
+	printArray(square)
 
 	cols := getCols(square)
-	fmt.Println(cols)
+
+	square = reduceArray(square)
+	cols = reduceArray(cols)
+
+	sortedArray := joinArray(square)
+	sortedCols := joinArray(cols)
+
+	fmt.Println(latinTest(sortedArray, sortedCols))
 
 }
 
-func arrayPrint(length *int, vals *string) [][]string {
+func parseArray(length *int, vals *string) [][]string {
 	arrayStr := strings.Split(*vals, " ")
 	temp := make([]string, 0)
 	var array [][]string
@@ -62,4 +82,67 @@ func getCols(array [][]string) [][]string {
 	}
 
 	return cols
+}
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func canonicalize(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
+
+func printArray(array [][]string) {
+	for _, v := range array {
+		fmt.Println(v)
+	}
+	fmt.Println()
+}
+
+func reduceArray(array [][]string) [][]string {
+
+	temp := make([]string, 0)
+	var reduced [][]string
+
+	for i := range array {
+		tmp := ""
+		for _, v := range array[i] {
+			tmp += v
+		}
+		tmp = canonicalize(tmp)
+		for j := range tmp {
+			temp = append(temp, string(tmp[j]))
+		}
+		reduced = append(reduced, temp)
+		temp = make([]string, 0)
+	}
+
+	return reduced
+}
+
+func joinArray(array [][]string) []string {
+	var vals []string
+	for _, v := range array {
+		vals = append(vals, strings.Join(v, ""))
+	}
+	return vals
+}
+
+func latinTest(array1, array2 []string) bool {
+	for i := range array1 {
+		if array1[i] != array2[i] {
+			return false
+		}
+	}
+	return true
 }

7d71a004baf4707370de2c76f8da155caa6ced04
Stephen Mahon <stemahon@gmail.com>
2021-10-04 23:11:40 -0500

    testing cols func

--- 328-Latin-Squares/latinSquares.go
+++ 328-Latin-Squares/latinSquares.go
@@ -15,14 +15,18 @@ type grid struct {
 
 func main() {
 	fmt.Println(title)
-	length := flag.Int("length", 0, "the length, n, of the array.")
-	array := flag.String("array", "", "the array matrix in the form n x n where n is the length of the array.")
+	length := flag.Int("length", 5, "the length, n, of the array.")
+	array := flag.String("array", "1 2 3 4 5 5 1 2 3 4 4 5 1 2 3 3 4 5 1 2 2 3 4 5 1", "the array matrix in the form n x n where n is the length of the array.")
 	flag.Parse()
 
 	square := arrayPrint(length, array)
 	for _, v := range square {
 		fmt.Println(v)
 	}
+
+	cols := getCols(square)
+	fmt.Println(cols)
+
 }
 
 func arrayPrint(length *int, vals *string) [][]string {
@@ -43,4 +47,19 @@ func arrayPrint(length *int, vals *string) [][]string {
 
 func getVal(array [][]string, x, y int) string {
 	return array[x][y]
+}
+
+func getCols(array [][]string) [][]string {
+	length := len(array)
+	temp := make([]string, 0)
+	var cols [][]string
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			temp = append(temp, array[j][i])
+		}
+		cols = append(cols, temp)
+		temp = make([]string, 0)
+	}
+
+	return cols
 }

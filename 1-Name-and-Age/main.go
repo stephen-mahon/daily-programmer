9ce1b6562f4ad00ac0ea58a888d0cca236f2f7f4
Stephen Mahon <stemahon@gmail.com>
2021-10-19 23:29:55 -0500

    matched scope in readmen

--- 1-Name-and-Age/main.go
+++ 1-Name-and-Age/main.go
@@ -1,60 +1,21 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strconv"
-	"time"
 )
 
+var title = "Name and age"
+
 func main() {
-	name := getName()
-	age := getAge()
+	name := flag.String("name", "Stephen", "Your name.")
+	age := flag.Int("age", 1, "Your age.")
+	twitter := flag.String("twitter", "stiofanomathuna", "Your Twitter handle")
+	flag.Parse()
 
-	fmt.Println("Hello,", name, ".")
-	fmt.Println("You are", age, "years old.")
+	fmt.Printf("your name is %s, you are %v years old, and your twitter name is %s.\n", *name, *age, twitterHandle(twitter))
 }
 
-func getName() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("What's your name? ")
-	name, _ := reader.ReadString('\n')
-	return name[:len(name)-1]
-}
-
-func getAge() int {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("What year where you born? ")
-	year, _ := reader.ReadString('\n')
-	year = year[:len(year)-1]
-	yearOfBirth, err := strconv.Atoi(year)
-	if err != nil {
-		panic(err)
-	}
-	t := time.Now()
-	currentYear := t.Year()
-	return currentYear - yearOfBirth
-}
-
-func getTwitterName() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("What is your Twitter name? ")
-	twitterName, _ := reader.ReadString('\n')
-	return twitterName[:len(twitterName)-1]
-}
-
-func getTweet(twitterName string) string {
-	twitterLink := "http://www.twitter.com/" + twitterName
-	page, err := http.Get(twitterLink)
-	if err != nil {
-		panic(err)
-	}
-	content, err := ioutil.ReadAll(page.Body)
-	if err != nil {
-		panic(err)
-	}
-	return string(content)
+func twitterHandle(ID *string) string {
+	return "@" + *ID
 }

a0cd5ee64aabe6a0e347bd6f5a8221d6af9bf8ef
Stephen Mahon <stemahon@gmail.com>
2021-10-24 23:02:42 -0500

    added time check and simple UI

--- intermediate/1-Event_Hours/main.go
+++ intermediate/1-Event_Hours/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"sort"
@@ -11,7 +12,8 @@ import (
 
 /*
 	To Do
-		[ ] user input time check
+		[x] user input time check
+		[ ] add edit
 */
 
 var title = "Events organizable by hour"
@@ -23,39 +25,29 @@ type event struct {
 
 var schedule = []event{}
 
-func main() {
-	fmt.Println(title)
-	var morning event
-	morning.hour = 8
-	morning.desc = "Coffee"
-
-	var run event
-	run.hour = 6
-	run.desc = "5k run"
-
-	morning.Add()
-	run.Add()
-
-	input()
-
-	print(schedule)
-
-}
-
-func (op *event) Add() {
-	schedule = append(schedule, *op)
-}
-
-func input() {
+func newEvent() {
 	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print("Description: ")
 	text, _ := reader.ReadString('\n')
 	fmt.Print("Time: ")
 	t, _ := reader.ReadString('\n')
+
 	t = strings.TrimSuffix(t, "\r\n")
 	time, _ := strconv.Atoi(t)
-	userEvent := event{time, text}
-	userEvent.Add()
+	_, err := timeCheck(time)
+
+	if err != nil {
+		fmt.Println(err)
+		newEvent()
+	} else {
+		userEvent := event{time, text}
+		userEvent.Add()
+	}
+}
+
+func (op *event) Add() {
+	schedule = append(schedule, *op)
 }
 
 func print(all []event) {
@@ -66,4 +58,47 @@ func print(all []event) {
 	for _, v := range all {
 		fmt.Printf("%v, %v\n", v.hour, v.desc)
 	}
+}
+
+func timeCheck(hour int) (int, error) {
+	if hour < 0 || hour > 23 {
+		err := fmt.Sprintf("incorrect time: \"%v\". use 24 hour clock.", hour)
+		return -1, errors.New(err)
+	}
+	return hour, nil
+}
+
+func trimReadString(in string) string {
+	return strings.TrimSuffix(in, "\r\n")
+}
+
+func userInterface(exit bool) bool {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("New Event (N) | View Schedule (V) | Quit (Q)")
+	input, _ := reader.ReadString('\n')
+
+	switch strings.ToUpper(trimReadString(input)) {
+	case "N":
+		newEvent()
+		return false
+	case "V":
+		print(schedule)
+		return false
+	case "Q":
+		return true
+	default:
+		return false
+	}
+}
+
+func main() {
+	fmt.Println(title)
+	exit := false
+
+	for !exit {
+		exit = userInterface(exit)
+		fmt.Println()
+	}
+
 }

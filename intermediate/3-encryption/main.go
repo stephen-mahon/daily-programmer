f9daff2da9146188dd2bf12174cec0d072a98130
Stephen Mahon <stemahon@gmail.com>
2021-10-26 23:29:28 -0500

    quick proof of principle

--- intermediate/3-encryption/main.go
+++ intermediate/3-encryption/main.go
@@ -1,9 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-var title = "Encryption"
+var title = "the quick brown fox jumps over thirteen lazy dogs"
+
+var m = map[string]string{
+	"a": "z",
+	"b": "y",
+	"c": "x",
+	"d": "w",
+	"e": "v",
+	"f": "u",
+	"g": "t",
+	"h": "s",
+	"i": "r",
+	"j": "q",
+	"k": "p",
+	"l": "o",
+	"m": "m",
+	"n": "m",
+	"o": "l",
+	"p": "k",
+	"q": "j",
+	"r": "i",
+	"s": "h",
+	"t": "g",
+	"u": "f",
+	"v": "e",
+	"w": "d",
+	"x": "c",
+	"y": "b",
+	"z": "a",
+	" ": " ",
+}
 
 func main() {
 	fmt.Println(title)
+	text := ciphertext(title)
+	fmt.Println(text)
+	text = ciphertext(text)
+	fmt.Println(text)
+}
+
+func ciphertext(plaintext string) string {
+	var text string
+	for i := range plaintext {
+		text += m[string(plaintext[i])]
+	}
+
+	return text
 }

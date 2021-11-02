93c08e1f55aceead8db5f4f0297d1032710f4558
Stephen Mahon <stemahon@gmail.com>
2021-11-01 23:37:21 -0500

    changed to combination function

--- easy/323-3SUM/3sum.go
+++ easy/323-3SUM/3sum.go
@@ -2,30 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 )
+
+// still need to extract unique combinations in set
 
 var title = "3SUM"
 var inputExamples = []int{9, -6, -5, 9, 8, 3, -4, 8, 1, 7, -4, 9, -9, 1, 9, -9, 9, 4, -6, -8}
 
 func main() {
 	fmt.Println(title)
-	//sort.Ints(inputExamples)
-	combinations(inputExamples)
-}
-
-func combinations(vals []int) [][]int {
-
-	//var triples [][3]int
-
+	vals := combinations(inputExamples, 3)
 	for i := range vals {
-		for j := range vals {
-			for k := range vals {
-				if vals[i]+vals[j]+vals[k] == 0 {
-					fmt.Println(vals[i], vals[j], vals[k])
-				}
-			}
+		if vals[i][0]+vals[i][1]+vals[i][2] == 0 {
+			fmt.Println(vals[i])
 		}
 	}
+}
 
-	return [][]int{}
+// Combinations returns combinations of n elements for a given string array.
+// For n < 1, it equals to All and returns all combinations.
+func combinations(set []int, n int) (subsets [][]int) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	// Go through all possible combinations of objects
+	// from 1 (only first object in subset) to 2^length (all objects in subset)
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []int
+
+		for object := uint(0); object < length; object++ {
+			// checks if object is contained in subset
+			// by checking if bit 'object' is set in subsetBits
+			if (subsetBits>>object)&1 == 1 {
+				// add object to subset
+				subset = append(subset, set[object])
+			}
+		}
+		// add subset to subsets
+		subsets = append(subsets, subset)
+	}
+	return subsets
 }

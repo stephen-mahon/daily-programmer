61b5d6757e496dbfbd1969198ab8a904fbf0eddc
Stephen Mahon <stemahon@gmail.com>
2021-11-02 19:51:58 -0500

    base func

--- easy/322-All-Pairs-Test-Generator/main.go
+++ easy/322-All-Pairs-Test-Generator/main.go
@@ -4,6 +4,24 @@ import "fmt"
 
 var title = "All pairs test generator"
 
+var testInput = [][]string{{"0", "1"}, {"A", "B", "C"}, {"D", "E", "F", "G"}}
+
 func main() {
 	fmt.Println(title)
+	printSet(testInput)
+	fmt.Printf("\nTotal Combinations: %v", totalCombinations(testInput))
+}
+
+func printSet(input [][]string) {
+	for i := range input {
+		fmt.Printf("%v ", input[i])
+	}
+}
+
+func totalCombinations(input [][]string) int {
+	totalCom := 1
+	for i := range input {
+		totalCom *= len(input[i])
+	}
+	return totalCom
 }

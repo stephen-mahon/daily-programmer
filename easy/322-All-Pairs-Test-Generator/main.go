3d6f25802a69a9fb03ec9d326dc12e2a8350b71a
Stephen Mahon <stemahon@gmail.com>
2021-11-03 22:31:13 -0500

    added func from 323

--- easy/322-All-Pairs-Test-Generator/main.go
+++ easy/322-All-Pairs-Test-Generator/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/bits"
+)
 
 var title = "All pairs test generator"
 
@@ -24,4 +27,36 @@ func totalCombinations(input [][]string) int {
 		totalCom *= len(input[i])
 	}
 	return totalCom
+}
+
+// Combinations returns combinations of n elements for a given string array.
+// For n < 1, it equals to All and returns all combinations.
+func combinations(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	// Go through all possible combinations of objects
+	// from 1 (only first object in subset) to 2^length (all objects in subset)
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			// checks if object is contained in subset
+			// by checking if bit 'object' is set in subsetBits
+			if (subsetBits>>object)&1 == 1 {
+				// add object to subset
+				subset = append(subset, set[object])
+			}
+		}
+		// add subset to subsets
+		subsets = append(subsets, subset)
+	}
+	return subsets
 }

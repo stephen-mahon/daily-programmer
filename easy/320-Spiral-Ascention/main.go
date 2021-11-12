c19a7e0303ab47816cd341d2c16fb0bf6a5f1e3f
Stephen Mahon <stemahon@gmail.com>
2021-11-11 23:14:48 -0600

    adding index for maze pattern solving

--- easy/320-Spiral-Ascention/main.go
+++ easy/320-Spiral-Ascention/main.go
@@ -11,11 +11,28 @@ func main() {
 	fmt.Println(title)
 	square := flag.Int("square", 3, "The length of the square for the spiral")
 	flag.Parse()
-	val := 1
+
+	array := makeArray(square)
+	traverseArray(array)
+
+}
+
+func makeArray(square *int) [][]string {
+	var array [][]string
 	for i := 0; i < *square; i++ {
+		tmp := []string{}
 		for j := 0; j < *square; j++ {
-			fmt.Printf("%v ", val)
-			val++
+			tmp = append(tmp, "X")
+		}
+		array = append(array, tmp)
+	}
+	return array
+}
+
+func traverseArray(array [][]string) {
+	for i := range array {
+		for j := range array[i] {
+			fmt.Printf("(%v,%v) ", i, j)
 		}
 		fmt.Println()
 	}

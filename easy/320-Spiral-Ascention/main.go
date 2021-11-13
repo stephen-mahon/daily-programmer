27e2251592c86db61499be875d8fe091b0e167ae
Stephen Mahon <stemahon@gmail.com>
2021-11-12 23:27:36 -0600

    added traverse bool table

--- easy/320-Spiral-Ascention/main.go
+++ easy/320-Spiral-Ascention/main.go
@@ -7,33 +7,53 @@ import (
 
 var title = "Spiral Ascention"
 
+type coor struct {
+	x int
+	y int
+}
+
 func main() {
 	fmt.Println(title)
 	square := flag.Int("square", 3, "The length of the square for the spiral")
 	flag.Parse()
 
 	array := makeArray(square)
+	printArray(square, array)
 	traverseArray(array)
 
 }
 
-func makeArray(square *int) [][]string {
-	var array [][]string
-	for i := 0; i < *square; i++ {
-		tmp := []string{}
-		for j := 0; j < *square; j++ {
-			tmp = append(tmp, "X")
+func makeArray(square *int) []coor {
+	var array []coor
+
+	for i := 1; i <= *square; i++ {
+		for j := 1; j <= *square; j++ {
+			array = append(array, coor{i, j})
 		}
-		array = append(array, tmp)
 	}
+
 	return array
 }
 
-func traverseArray(array [][]string) {
-	for i := range array {
-		for j := range array[i] {
-			fmt.Printf("(%v,%v) ", i, j)
+func printArray(square *int, array []coor) {
+	for i := 1; i <= len(array); i++ {
+		if i != 0 && i%*square == 0 {
+			fmt.Printf("(%v,%v)\n", array[i-1].x, array[i-1].y)
+		} else {
+			fmt.Printf("(%v,%v) ", array[i-1].x, array[i-1].y)
 		}
-		fmt.Println()
 	}
+}
+
+func traverseArray(array []coor) {
+	visit := make([]bool, len(array))
+	fmt.Println(visit)
+	for i := range array {
+		if !visit[i] {
+			fmt.Println(array[i].x, array[i].y)
+			visit[i] = true
+		}
+
+	}
+	fmt.Println(visit)
 }

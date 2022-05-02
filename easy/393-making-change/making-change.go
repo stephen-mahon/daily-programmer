7b36d21f949c270629f53be984b7769ce5bce406
Stephen Mahon <stemahon@gmail.com>
2022-05-01 23:29:02 -0500

    added flags and removed args input

--- easy/393-making-change/making-change.go
+++ easy/393-making-change/making-change.go
@@ -1,39 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 1 && args[0] == "-help" || args[0] == "-h" {
-		fmt.Println("Making change")
-		fmt.Printf("usage:   ./making-change <int>\n")
-		fmt.Printf("example: ./making-change 123456\n")
-		fmt.Printf("output:  254\n")
+
+	change := flag.Int("change", 0, "making change")
+	flag.Parse()
+	if *change >= 0 {
+		fmt.Println(makeChange(*change))
 	} else {
-		if len(args) != 1 {
-			fmt.Printf("You must enter one argument! Type \"-help\" for help.\n")
-		} else {
-			res, _ := makeChange(args[0])
-			fmt.Println(res)
-		}
+		fmt.Println("change must be a postive value: ", *change)
+		os.Exit(1)
 	}
+
 }
 
-func makeChange(arg string) (int, error) {
-	val, err := strconv.Atoi(arg)
-	if err != nil {
-		return -1, err
-	}
+func makeChange(arg int) int {
 	coins := [6]int{500, 100, 25, 10, 5, 1}
 	var change int
 	for i := range coins {
-		res := val / coins[i]
+		res := arg / coins[i]
 		change += res
-		val -= res * coins[i]
+		arg -= res * coins[i]
 	}
-	return change, nil
+	return change
 }

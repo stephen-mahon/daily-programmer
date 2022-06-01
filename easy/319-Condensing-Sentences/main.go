409fbb3e85d53a78e8d49aa231032faae0e67755
Stephen Mahon <stemahon@gmail.com>
2022-05-31 22:37:51 -0500

    added parser

--- easy/319-Condensing-Sentences/main.go
+++ easy/319-Condensing-Sentences/main.go
@@ -20,12 +20,25 @@ func main() {
 
 	dat, err := os.ReadFile(*file)
 	check(err)
-	fmt.Println(string(dat))
-	//use regexp
+	_ = parseInput(dat)
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func parseInput(dat []byte) []string {
+	var out []string
+	var word string
+	for i := range dat {
+		if string(dat[i]) == " " {
+			out = append(out, word)
+			word = ""
+		}
+		word += string(dat[i])
+	}
+	out = append(out, word)
+	return out
 }

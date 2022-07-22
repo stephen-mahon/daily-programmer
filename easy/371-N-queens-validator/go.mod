6b91c1a1326a42a6782327fdcea28376c1d4c32c
Stephen Mahon <stemahon@gmail.com>
2022-07-22 16:12:58 -0500

    update

---
+++ easy/371-N-queens-validator/go.mod
@@ -0,0 +1,3 @@
+module github.com/daily-programmer/easy/371-N-queens-validator
+
+go 1.17
--- easy/371-N-queens-validator/main.go
+++ easy/371-N-queens-validator/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	pos := flag.String("s", "", "")
+	flag.Parse()
+	fmt.Println(*pos)
+}

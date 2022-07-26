f9b495eabac2161d4d97eada927f9bf72329be67
Stephen Mahon <stemahon@gmail.com>
2022-07-26 11:59:14 -0500

    Added parsing from cmd

---
+++ easy/369-Hex-colors/go.mod
@@ -0,0 +1,3 @@
+module github.com/stephen-mahon/daily-programmer/easy/369-Hex-colors
+
+go 1.17
---
+++ easy/369-Hex-colors/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	args := flag.String("c", "0 0 0", "rgb values")
+	flag.Parse()
+	rgb, err := parseNums(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rgb)
+}
+
+func parseNums(strInput *string) ([]int, error) {
+	strSplit := strings.Split(*strInput, " ")
+	if len(strSplit) != 3 {
+		return []int{}, fmt.Errorf("invalid input rgb value must be 3 numbers: %s", *strInput)
+	}
+	var vals []int
+	for i := 0; i < 3; i++ {
+		val, err := strconv.Atoi(strSplit[i])
+		if err != nil {
+			return []int{}, fmt.Errorf("invalid input rgb value must be a numbers: %s", strSplit[i])
+		}
+		if val < 0 || val > 255 {
+			return []int{}, fmt.Errorf("invalid input rgb value must be between 0 and 255: %v", val)
+		}
+		vals = append(vals, val)
+	}
+	return vals, nil
+}

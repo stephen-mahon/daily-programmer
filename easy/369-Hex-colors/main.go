3ddcce7feb9f89b66e2d9b493d68a39b393cdb01
Stephen Mahon <stemahon@gmail.com>
2022-07-27 15:47:27 -0500

    added hex representation to print the rgb

--- easy/369-Hex-colors/main.go
+++ easy/369-Hex-colors/main.go
@@ -15,7 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(rgb)
+	fmt.Printf("hexcolor(%v, %v, %v) => \"#%02x%02x%02x\"", rgb[0], rgb[1], rgb[2], rgb[0], rgb[1], rgb[2])
 }
 
 func parseNums(strInput *string) ([]int, error) {

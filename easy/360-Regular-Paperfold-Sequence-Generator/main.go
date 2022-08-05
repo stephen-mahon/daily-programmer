fd51050edf8addc06c1b18d2a15464e8b4de4553
Stephen Mahon <stemahon@gmail.com>
2022-08-04 22:51:52 -0500

    some error will fix soon

--- easy/360-Regular-Paperfold-Sequence-Generator/main.go
+++ easy/360-Regular-Paperfold-Sequence-Generator/main.go
@@ -5,14 +5,30 @@ import (
 	"fmt"
 )
 
-const seed = "11"
+const seed = "1"
 
 func main() {
-	cycles := flag.Int("c", 1, "number of cycles")
+	cycles := flag.Int("c", 2, "number of cycles")
 	flag.Parse()
 	fmt.Println(dragon(seed, *cycles))
 }
 
 func dragon(seed string, num int) string {
-	return ""
+	for num > 0 {
+		fmt.Printf("cycle #%v: %v -> ", num, seed)
+		seed = seed + seed + middleComplemented(seed)
+		fmt.Printf("%v\n", seed)
+		dragon(seed, num-1)
+	}
+	return seed
+}
+
+func middleComplemented(digits string) string {
+	middle := digits[(len(digits)-1)/2]
+	if digits[middle] == '1' {
+		digits = digits[0:middle] + "0" + digits[middle:]
+	} else {
+		digits = digits[0:middle] + "1" + digits[middle:]
+	}
+	return digits
 }

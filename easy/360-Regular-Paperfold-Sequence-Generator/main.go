9494d2d1265b24b700cfb5b71c64503a494e6bbd
Stephen Mahon <stemahon@gmail.com>
2022-08-06 22:47:24 -0500

    challenge 360 complete

--- easy/360-Regular-Paperfold-Sequence-Generator/main.go
+++ easy/360-Regular-Paperfold-Sequence-Generator/main.go
@@ -14,19 +14,27 @@ func main() {
 }
 
 func dragon(seed string, num int) string {
-	for num > 0 {
-		fmt.Printf("cycle #%v: %v -> ", num, seed)
-		seed = seed + seed + middleComplemented(seed)
-		fmt.Printf("%v\n", seed)
-		dragon(seed, num-1)
+	//fmt.Printf("%v\n", seed)
+	if num == 0 {
+		return seed
+	} else {
+		seed = seed + "1" + middleComplemented(seed)
+		seed = dragon(seed, num-1)
 	}
 	return seed
 }
 
 func middleComplemented(digits string) string {
-	middle := digits[(len(digits)-1)/2]
+	middle := (len(digits) - 1) / 2
+	if len(digits) == 1 {
+		if digits[0] == '1' {
+			return "0"
+		} else {
+			return "1"
+		}
+	}
 	if digits[middle] == '1' {
-		digits = digits[0:middle] + "0" + digits[middle:]
+		digits = digits[0:middle] + "0" + digits[middle+1:]
 	} else {
 		digits = digits[0:middle] + "1" + digits[middle:]
 	}

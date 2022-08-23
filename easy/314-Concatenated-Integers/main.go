a0dbd31a6803421b2e51b30e6255dab63da32570
Stephen Mahon <stemahon@gmail.com>
2022-08-22 23:42:00 -0500

    added heaps algorithm for generating permutations

--- easy/314-Concatenated-Integers/main.go
+++ easy/314-Concatenated-Integers/main.go
@@ -3,53 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"math"
 	"strconv"
 	"strings"
 )
 
 // To do
-//	[ ] sort out the bug with casting large ints to int32
+//	[ ] concat loop
 
 func main() {
 	args := flag.String("a", "", "a list of integers")
 	flag.Parse()
-	max, min, err := concatInt(*args)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(max, min)
+	vals := strings.Split(*args, " ")
+	heapsAlgorithm(vals, len(vals))
+
 }
 
-/// works fine for small numbers but the casting to int64 is causing some issues with the concat
-func concatInt(args string) (max, min int64, err error) {
-	input := strings.Split(args, " ")
-	max = math.MinInt32
-	min = math.MaxInt32
-	for i := range input {
-		for j := range input {
-			for k := range input {
-				if i != j && j != k && k != i {
-					// I think that here we are getting numbers larger than ints
-					v := input[i] + input[j] + input[k]
-					val, err := strconv.Atoi(v)
-					if err != nil {
-						return 0, 0, err
-					}
-					// I don't think that this casting is working well
-					if int64(val) > max {
-						// Here neither
-						max = int64(val)
-					}
-					// And here
-					if int64(val) < min {
-						// finally, here too
-						min = int64(val)
-					}
-				}
-			}
+func heapsAlgorithm(s []string, l int) {
+	if l == 1 {
+		tmp := ""
+		for i := range s {
+			tmp += s[i]
+		}
+		val, _ := strconv.Atoi(tmp)
+		fmt.Println(val)
+	}
+
+	for i := 0; i < l; i++ {
+		heapsAlgorithm(s, l-1)
+		if l%2 == 1 {
+			s[0], s[l-1] = s[l-1], s[0]
+		} else {
+			s[i], s[l-1] = s[l-1], s[i]
 		}
 	}
-	return max, min, nil
 }

3be71408aa927ba4e3a1cabb04536904729ee02d
Stephen Mahon <stemahon@gmail.com>
2022-08-28 15:53:26 -0500

    added parse input

--- easy/313-Subset-sum/main.go
+++ easy/313-Subset-sum/main.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+var arg string
 
 func main() {
-	vals := []int{}
-	fmt.Println(vals, "->", simpleSubsetSum(vals))
+	flag.StringVar(&arg, "o", "0", "arg inputs")
+	flag.Parse()
+	tail := flag.Args()
+	input, err := parseInput(arg, tail)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(input, "->", simpleSubsetSum(input))
 
+}
+
+func parseInput(head string, tail []string) ([]int, error) {
+	var vals []int
+	val, err := strconv.Atoi(head)
+	if err != nil {
+		return []int{}, err
+	}
+	vals = append(vals, val)
+	for _, v := range tail {
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			return []int{}, err
+		}
+		vals = append(vals, val)
+	}
+	return vals, nil
 }
 
 func simpleSubsetSum(vals []int) bool {
